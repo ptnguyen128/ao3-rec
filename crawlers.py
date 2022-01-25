25ad4d54261fd261f4460b00a94dd60d15c19b79
Thao Nguyen <ptnguyen128@gmail.com>
2022-01-25 15:58:49 -0600

    set os environ args

--- crawlers.py
+++ crawlers.py
@@ -1,9 +1,9 @@
 import sys
 import os
 import boto3
+from dotenv import dotenv_values
 
 from src.helpers import _s3_file_exists
-from _creds import aws_creds
 
 import streamlit as st
 from scrapy.crawler import CrawlerProcess
@@ -15,19 +15,24 @@ def bookmarks_crawler(username):
     Given a username, scrape user's bookmark page
     and write a json file to output
     """
+    if os.path.exists('.env'):
+        config = dotenv_values(".env")
+        for k, v in config.items():
+            os.environ[k] = v
+
     with st.spinner("Just a little bit..."):
         # check if file already existed on S3
         s3_client = boto3.client('s3',
-                                 aws_access_key_id=aws_creds['access_key_id'],
-                                 aws_secret_access_key=aws_creds['secret_access_key'])
-        bucket = 'streamlit-ao3-data'
+                                 aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+                                 aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+        bucket = os.environ['S3_BUCKET_NAME']
         file_key = f'bookmarks/{username}.txt'
         if not _s3_file_exists(s3_client, bucket, file_key):
             process = CrawlerProcess(settings={
                 "FEED_URI": f's3://streamlit-ao3-data/bookmarks/{username}.txt',
                 "FEED_FORMAT": 'json',
-                "AWS_ACCESS_KEY_ID": aws_creds['access_key_id'],
-                "AWS_SECRET_ACCESS_KEY": aws_creds['secret_access_key'],
+                "AWS_ACCESS_KEY_ID": os.environ['AWS_ACCESS_KEY_ID'],
+                "AWS_SECRET_ACCESS_KEY": os.environ['AWS_SECRET_ACCESS_KEY'],
                 }
             )
             process.crawl(BookmarkScraper,
