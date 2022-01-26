68ce5b8b2191c2a4e933e0d378be711361800fcd
Thao Nguyen <ptnguyen128@gmail.com>
2022-01-26 01:20:18 -0600

    specific columns

--- src/bookmarks_stats.py
+++ src/bookmarks_stats.py
@@ -44,10 +44,11 @@ def get_bookmarks_stats(username, oneshot_only, completed_only,
     else:
         # transform columns
         for c in meta_df.columns:
-            if c not in ['author', 'fandom', 'pairings', 'tags', 'category']:
+            if c in ['title', 'work_id', 'work_url', 'status', 'summary',
+                     'language', 'published', 'chapters', 'rating']:
                 meta_df[c] = meta_df[c].apply(lambda x: ''.join(x))
-                if c in ['words', 'comments', 'bookmarks', 'kudos', 'hits']:
-                    meta_df[c] = meta_df[c].astype(float)
+            if c in ['words', 'comments', 'bookmarks', 'kudos', 'hits']:
+                meta_df[c] = meta_df[c].astype(float)
 
         # oneshot filter
         if oneshot_only:
